91ee855696f7960c336ea03e5f2fb8c6d51355c6
ABHINAND ULLAS P <145597216+Abhinand-Ullas@users.noreply.github.com>
2025-01-15 17:29:04 +0530

    home page changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,21 +6,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: HomeBody(),
-      bottomNavigationBar: BottomNavigationBar(items: [
-
-        BottomNavigationBarItem(icon: Icon(Icons.explore),
-        label: 'Explore'),
-
-        BottomNavigationBarItem(icon: Image.asset("images/support.png"),
-        label: 'Support'),
-        BottomNavigationBarItem(icon: Image.asset("images/updates.png"),
-        label: 'Updates'),
-        BottomNavigationBarItem(icon: Image.asset("images/report.png"),
-        label: 'Report'),
-        
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          selectedLabelStyle:
+              TextStyle(fontWeight: FontWeight.bold, color: Colors.amber),
+          showSelectedLabels: true, // Add this line
+          showUnselectedLabels: true, // Add this line
+          selectedItemColor:
+              Colors.black, // Change selected label color to black
+          unselectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+                icon:
+                    Icon(Icons.explore, color: Color.fromARGB(255, 74, 68, 88)),
+                label: 'Explore'),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  "assets/images/support.png",
+                  height: 20,
+                  width: 20,
+                ),
+                label: 'support'),
+            BottomNavigationBarItem(
+                icon: Image.asset("assets/images/updates.png"),
+                label: 'Updates'),
+            BottomNavigationBarItem(
+                icon: Image.asset("assets/images/report.png"), label: 'Report'),
+          ]),
     );
   }
 }
